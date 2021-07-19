f3e8193c1e35b191acd95ba576b9eef5106ea3e0
Scot Hampel <scot@scothampel.com>
2021-07-19 19:38:44 -0400

    Start front-end Nothing really got done here

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState, useEffect} from 'react';
+import { useState } from 'react';
+import './css/App.css';
 
 function App() {
-  const [data, setData] = useState();
-
-  useEffect(() => {
-    fetch('/api')
-      .then(res => res.json())
-      .then(data => setData(data));
-  }, []);
+  const [user, setUser] = useState();
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {!data ? 'Loading...' : data.token}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
---
+++ client/src/components/Login.js
@@ -0,0 +1,9 @@
+import '../css/Login.css';
+
+export default function Login() {
+  return (
+    <div className='container' id='login'>
+      
+    </div>
+  )
+}
