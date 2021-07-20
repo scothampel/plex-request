7ca789e6f1735c106c6287c535ed8b3c2ba165d3
Scot Hampel <scot@scothampel.com>
2021-07-20 15:49:36 -0500

    Remove Link import, unused

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Login from './Login';
 import '../css/App.css';
 
