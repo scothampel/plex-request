bf04aef8ed701ae69ce372a05ccfc77bab613258
Scot Hampel <scot@scothampel.com>
2021-07-20 21:02:34 -0500

    Remove jwt import

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
-const jwt = require('jsonwebtoken');
 const path = require('path');
 const { MongoClient } = require('mongodb');
 const cookieParser = require('cookie-parser');
