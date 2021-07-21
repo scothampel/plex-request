328af5f11f259218ff8c206e7c4b0ee15f5dea2a
Scot Hampel <scot@scothampel.com>
2021-07-21 12:04:50 -0500

    Commenting

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -4,6 +4,7 @@ export default function Logout({ setToken }) {
   // TODO: Maybe just make this a function in app, not sure yet
   useEffect(() => {
     // Remove client auth token, delete refresh token from db
+    // Could potentially not get logged out if server down, non issue at the moment
     setToken(null)
     fetch('/auth/logout', {method: 'POST'})
       .catch(err => console.error('Internal Server Error', err));
