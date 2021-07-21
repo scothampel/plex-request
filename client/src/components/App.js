239eecc9ba596a3a5df3fcb6afe0ebc155cb2776
Scot Hampel <scot@scothampel.com>
2021-07-20 23:40:03 -0500

    Formatting and comments

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -9,23 +9,29 @@ function App() {
   const [token, setToken] = useState();
   const [loading, setLoading] = useState(true);
 
+  // Pull new token if refesh cookie is set (logged in)
+  // Can not check for refresh cookie, as it is httpOnly
   useEffect(() => {
+    // First load, no token and page is loading
     if (!token && loading) {
-      fetch('/auth/refresh', {
-        method: 'POST'
-      })
+      fetch('/auth/refresh', {method: 'POST'})
         .then(res => res.json())
         .then(data => {
           const { status, message } = data
+          // Check if auth token is returned
           if (status === 1) {
             setToken(message)
           }
+          // Fetch complete, allow proper components to render
           setLoading(false);
         })
         .catch(err => console.error('Internal Server Error', err));
     }
+  // Deps set so this fetch only runs when visiting the page for the first time
+  // Also runs on refresh, might also store auth token in a cookie to prevent this
   }, [token, loading]);
 
+  // TODO: Loading splash screen instead of text
   return (
     <Router>
       <Switch>
@@ -39,6 +45,7 @@ function App() {
           {token ? <Logout setToken={setToken} /> : <Redirect to='/' />}
         </Route>
         <Route path='/'>
+          {/* Temp for testing */}
           {loading && 'Loading...'}
           {!loading && token &&
             <div>
--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -1,12 +1,11 @@
 import { useEffect } from 'react';
 
 export default function Logout({ setToken }) {
+  // TODO: Maybe just make this a function in app, not sure yet
   useEffect(() => {
+    // Remove client auth token, delete refresh token from db
     setToken(null)
-
-    fetch('/auth/logout', {
-      method: 'POST'
-    })
+    fetch('/auth/logout', {method: 'POST'})
       .catch(err => console.error('Internal Server Error', err));
   })
 
