fdf2b40fe9649f0ff6944bfec0a69253c0ffd5b0
Scot Hampel <scot@scothampel.com>
2021-07-25 15:51:58 -0500

    Link not needed

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Login from './Login';
 import Register from './Register';
 import Logout from './Logout';
