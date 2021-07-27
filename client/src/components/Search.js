83fdf450de5d11c6de0a246681e70dca2db6d79d
Scot Hampel <scot@scothampel.com>
2021-07-26 23:57:45 -0500

    Input to only list-group-item

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -46,9 +46,9 @@ export default function Search({ token, setNewRequest }) {
   }
 
   return (
-    <ul className='list-group mb-3'>
+    <ul className='list-group mb-3 search'>
       <li className='list-group-item bg-light'><h3>Search</h3></li>
-      <input type='text' className='form-control list-group-item rounded-0 shadow-none border border-top-0' onChange={handleChange} placeholder='ex. Rick and Morty' />
+      <input type='text' className='list-group-item' onChange={handleChange} placeholder='ex. Rick and Morty' />
       {
         results.map((val, index) => {
           return (
