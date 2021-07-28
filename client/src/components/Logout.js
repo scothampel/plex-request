87d174eebc3f64a7b45e2fd49c54b1c416140679
Scot Hampel <scot@scothampel.com>
2021-07-28 12:51:02 -0500

    Redirect to login on register Remove logout TODO

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 
 export default function Logout({ setToken, setRole, setNeedLogin }) {
-  // TODO: Maybe just make this a function in app, not sure yet
   useEffect(() => {
     // Remove client auth token, delete refresh token from db
     // Could potentially not get logged out if server down, non issue at the moment
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,8 +1,16 @@
 import '../styles/Register.css';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 // TODO: redirect on register
 export default function Register() {
+  const [registered, setRegistered] = useState(false);
+
+  // Reset registered
+  useEffect(() => {
+    setRegistered(false);
+  }, [setRegistered])
+
   const handleSubmit = e => {
     // Prevent submission
     e.preventDefault();
@@ -28,6 +36,8 @@ export default function Register() {
           // Successful register
           case 1:
             alert('Successfully registered!');
+            // Set registered so user is redirected to login
+            setRegistered(true);
             break;
           // Bad input case 0, server error case -1
           default:
@@ -39,23 +49,30 @@ export default function Register() {
   }
 
   return (
-    <div className='container' id='login'>
-      <form onSubmit={handleSubmit}>
-        <div className='mb-3'>
-          <label htmlFor='name' className='form-label'>Name</label>
-          <input type='text' className='form-control' id='name' name='name' required />
+    <>
+      {/* Redirect if registered */}
+      {registered && <Redirect to='/login' />}
+      {
+        !registered &&
+        <div className='container' id='login'>
+          <form onSubmit={handleSubmit}>
+            <div className='mb-3'>
+              <label htmlFor='name' className='form-label'>Name</label>
+              <input type='text' className='form-control' id='name' name='name' required />
+            </div>
+            <div className='mb-3'>
+              <label htmlFor='user' className='form-label'>Username</label>
+              <input type='text' className='form-control' id='user' name='user' required />
+            </div>
+            <div className='mb-3'>
+              <label htmlFor='pass' className='form-label'>Password</label>
+              <input type='password' className='form-control' id='pass' name='pass' required />
+            </div>
+            <button type="submit" className="btn btn-primary me-3">Register</button>
+            <Link to='/login'>Already have an Account? Login here</Link>
+          </form>
         </div>
-        <div className='mb-3'>
-          <label htmlFor='user' className='form-label'>Username</label>
-          <input type='text' className='form-control' id='user' name='user' required />
-        </div>
-        <div className='mb-3'>
-          <label htmlFor='pass' className='form-label'>Password</label>
-          <input type='password' className='form-control' id='pass' name='pass' required />
-        </div>
-        <button type="submit" className="btn btn-primary me-3">Register</button>
-        <Link to='/login'>Already have an Account? Login here</Link>
-      </form>
-    </div>
+      }
+    </>
   )
 }
