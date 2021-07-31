4f41b1f6c60eb94fcf2e047cc929c0a77957275d
Scot Hampel <scot@scothampel.com>
2021-07-31 12:02:56 -0500

    COntainer instead of fluid

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,7 +4,7 @@ import '../styles/Navbar.css';
 export default function Navbar() {
   return (
     <nav className='navbar navbar-light bg-light mb-3 shadow-sm'>
-      <div className='container-fluid'>
+      <div className='container'>
         <Link to='/' className='navbar-brand'>Plex Requests</Link>
         <Link to='/logout' className='btn btn-primary'>Logout</Link>
       </div>
