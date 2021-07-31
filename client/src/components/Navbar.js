a8e7d4e1d9062bab90951d7520c7af7b1eefce2f
Scot Hampel <scot@scothampel.com>
2021-07-31 12:42:05 -0500

    Theme colors and styles

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,10 +3,10 @@ import '../styles/Navbar.css';
 
 export default function Navbar() {
   return (
-    <nav className='navbar navbar-light bg-light shadow-sm'>
+    <nav className='navbar navbar-dark bg-theme-800 shadow-sm'>
       <div className='container'>
         <Link to='/' className='navbar-brand'>Plex Requests</Link>
-        <Link to='/logout' className='btn btn-primary'>Logout</Link>
+        <Link to='/logout' className='btn btn-theme-accent'>Logout</Link>
       </div>
     </nav>
   );
